7285a4dedaae4a32199201fe625a743c5b08c6e2
Graeme Coupar <grambo@grambo.me.uk>
2016-07-03 16:44:24 +0100

    Add long description & links in setup.py

--- setup.py
+++ setup.py
@@ -3,11 +3,22 @@
 from setuptools import setup, find_packages
 
 REQUIREMENTS = ['websockets']
+LONG_DESCRIPTION = '''
+Chunnel
+-----
+
+A python client for phoenix channels.
+
+See the README at https://github.com/obmarg/chunnel for more details.
+
+'''
 
 setup(
     name='chunnel',
     version='0.1.0',
+    url='https://github.com/obmarg/chunnel',
     description='Phoenix channels client library',
+    long_description=LONG_DESCRIPTION,
     author='Graeme Coupar',
     author_email='grambo@grambo.me.uk',
     packages=find_packages(exclude=['tests']),
